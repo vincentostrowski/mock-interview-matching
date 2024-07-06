2767f4be7529fd7053395770f068ba908ddb8258
vincentostrovski <92182422+vincentostrovski@users.noreply.github.com>
2024-07-06 14:11:34 -0400

    Initialize Spring Boot server with basic dependencies

---
+++ server/src/main/java/com/example/interviewmatching/InterviewMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewMatchingApplication.class, args);
+	}
+
+}
